f6c0aee23671be6b6975b77cb4e46df2b0e04c0e
wrddgg <1263121100@qq.com>
2026-08-20 11:28:51 +0800

    chore: 版本 1.1.0（V0.2 首轮）

--- src/shared/contracts.ts
+++ src/shared/contracts.ts
@@ -1,4 +1,4 @@
-export const DESKTOP_VERSION = '1.0.0'
+export const DESKTOP_VERSION = '1.1.0'
 export const DSH_VERSION = '0.1.0-rc.7'
 export const RELEASES_URL = 'https://github.com/wrddgg/DSH-Desktop/releases'
 
